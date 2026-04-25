22a0016dd444a338fdd3e12c52874b4beebfd3a3
deilAdrineda <adrinedajohndeil@gmail.com>
2026-04-26 01:55:41 +0800

    version 2
    Changed the behavior/design of navigational header/bar. For both mobile and desktop view.

--- main.js
+++ main.js
@@ -33,30 +33,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-    // window.addEventListener("scroll", () => {
-    // let current = "";
-
-    // sections.forEach(section => {
-    //     const sectionTop = section.offsetTop;
-    //     const sectionHeight = section.clientHeight;
-
-    //     if (
-    //     window.scrollY >= sectionTop - sectionHeight / 3 &&
-    //     window.scrollY < sectionTop + sectionHeight - sectionHeight / 3
-    //     ) {
-    //     current = section.getAttribute("id");
-    //     }
-    // });
-
-    // navLinks.forEach(link => {
-    //     link.classList.remove("active");
-
-    //     if (link.getAttribute("href") === `#${current}`) {
-    //     link.classList.add("active");
-    //     }
-    // });
-    // });
-
   // 🔹 SECTION FOCUS EFFECT
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
